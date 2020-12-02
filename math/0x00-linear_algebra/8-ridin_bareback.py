bfb9db5cde942ac0f2ae04549e80ec4bae265de9
laucavv <laucavv23@gmail.com>
2020-12-02 09:58:03 -0500

    adding else

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -11,4 +11,6 @@ def mat_mul(mat1, mat2):
             for j in range(len(mat2[0])):
                 for k in range(len(mat2)):
                     new_mat[i][j] += mat1[i][k] * mat2[k][j]
-    return new_mat
+        return new_mat
+    else:
+        return None
