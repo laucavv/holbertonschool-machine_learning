ddc89f5afd8e8c863ce878d06e1b487e0e94a752
laucavv <laucavv23@gmail.com>
2020-12-02 09:56:32 -0500

    change line 11 mat2 -> mat2[0]

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -8,7 +8,7 @@ def add_matrices2D(mat1, mat2):
     if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
         for i in range(len(mat1)):
             sum = []
-            for j in range(len(mat2)):
+            for j in range(len(mat2[0])):
                 sum.append(mat1[i][j] + mat2[i][j])
             sum_matirx.append(sum)
         return sum_matirx
