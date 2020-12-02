deca61cb9c4582af931afda9e8da334c133c61c9
laucavv <laucavv23@gmail.com>
2020-12-01 22:24:11 -0500

    adds two matrices element-wise

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" adds two arrays element-wise """
+
+
+def add_matrices2D(mat1, mat2):
+    """  adds two arrays element-wise: """
+    sum_matirx = []
+    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
+        for i in range(len(mat1)):
+            sum = []
+            for j in range(len(mat2)):
+                sum.append(mat1[i][j] + mat2[i][j])
+            sum_matirx.append(sum)
+        return sum_matirx
+
+    return None
