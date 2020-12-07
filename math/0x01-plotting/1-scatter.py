96bf4a6fdffe2d97bc797b3f59c93b51da388db2
laucavv <laucavv23@gmail.com>
2020-12-07 09:51:50 -0500

    scatter plot

---
+++ math/0x01-plotting/1-scatter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+mean = [69, 0]
+cov = [[15, 8], [8, 15]]
+np.random.seed(5)
+x, y = np.random.multivariate_normal(mean, cov, 2000).T
+y += 180
+# label
+plt.ylabel('Weight (lbs)')
+plt.xlabel('Height (in)')
+plt.title('Men\'s Height vs Weight')
+
+# create scatter plot
+plt.scatter(x, y, s=10, color='magenta')
+plt.show()
