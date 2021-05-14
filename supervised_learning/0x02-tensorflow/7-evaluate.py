6759b8c7cfd30d6442ed19e2c1c56478d85c9a45
laucavv <laucavv23@gmail.com>
2021-05-13 21:09:55 -0500

    tensorflow

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Tensorflow module """
+
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """ Function that evaluates the output of a neural network. """
+
+    with tf.Session() as sess:
+
+        saver = tf.train.import_meta_graph('{}.meta'.format(save_path))
+        saver.restore(sess, '{}'.format(save_path))
+
+        x, *_ = tf.get_collection('x')
+        y, *_ = tf.get_collection('y')
+        y_pred, *_ = tf.get_collection('y_pred')
+        loss, *_ = tf.get_collection('loss')
+        accuracy, *_ = tf.get_collection('accuracy')
+
+        predictionOutput = sess.run(y_pred, feed_dict={x: X, y: Y})
+        lossOutput = sess.run(loss, feed_dict={x: X, y: Y})
+        accuracyOutput = sess.run(accuracy, feed_dict={x: X, y: Y})
+
+    return predictionOutput, accuracyOutput, lossOutput
---
+++ supervised_learning/0x02-tensorflow/README.md
@@ -0,0 +1,11 @@
+0x02. Tensorflow
+
+Task
+0. Placeholders
+1. Layers
+2. Forward Propagation
+3. Accuracy
+4. Loss
+5. Train_Op
+6. Train
+7. Evaluate
